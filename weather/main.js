384b914614e7ee71e72ec081f05075e524bd95ac
tylersullivan21 <tyler.sullivan2198@knights.ucf.edu>
2020-04-16 17:57:29 -0400

    got api data working

--- weather/main.js
+++ weather/main.js
@@ -1,11 +1,31 @@
 var weather = require('openweather-apis');
 weather.setLang('en');
-weather.setZipCode(32608);
+weather.setCity('Tokyo');
 
-weather.setAPPID(7eaa7caf5e2e619bfeb2e2d14839650d);
+weather.setAPPID('7eaa7caf5e2e619bfeb2e2d14839650d');
 
-// get the Temperature  
-weather.getTemperature(function(err, temp){
-    alert(temp);
-});
 
+    // get the Temperature  
+    weather.getTemperature(function(err, temp){
+        console.log(temp);
+    });
+ 
+    // get the Atm Pressure
+    weather.getPressure(function(err, pres){
+        console.log(pres);
+    });
+ 
+    // get the Humidity
+    weather.getHumidity(function(err, hum){
+        console.log(hum);
+    });
+ 
+    // get the Description of the weather condition
+    weather.getDescription(function(err, desc){
+        console.log(desc);
+    });
+ 
+    // get all the JSON file returned from server (rich of info)
+    weather.getAllWeather(function(err, JSONObj){
+        console.log(JSONObj);
+    });
